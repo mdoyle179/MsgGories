71e6cd1d1401cd1d2bb82a8e0ee98556e3eaa137
airlanda <agdel.m.irlanda@lmco.com>
2018-11-14 10:43:09 -0500

    Added a few files to text

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const items = require("./routes/api/items");
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+//Db Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/items", items);
+
+app.listen(port, () => console.log("Server started on port " + port));
